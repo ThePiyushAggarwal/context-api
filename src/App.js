e5d081a642c2753aa701055a803f2b2c7e4ec766
Piyush Aggarwal <piyushaggarwal801@gmail.com>
2022-04-20 17:40:31 +0530

    learnt using context

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-import React from 'react'
+import Nav from './components/Nav'
+import Products from './components/Products'
+import Checkout from './components/Checkout'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { CartProvider } from './CartContext'
 
 export default function App() {
-  return <div>App</div>
+  return (
+    <div className="App">
+      <CartProvider>
+        <BrowserRouter>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Products />} />
+            <Route path="/checkout" element={<Checkout />} />
+          </Routes>
+        </BrowserRouter>
+      </CartProvider>
+    </div>
+  )
 }
